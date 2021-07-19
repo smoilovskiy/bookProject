2f98d75f9d765c85d67fcb17b9e45c392c9584b4
Rostislav Smoilovskiy <smoilovskiy@gmail.com>
2021-07-19 17:20:47 +0300

    add template objects array

---
+++ js/main.js
@@ -0,0 +1,23 @@
+let arr = [
+  {
+   'author' : {},
+   'avatar' : 'img/avatars/user{{xx}}.png',
+   'offer' : {},
+   'title' : '',
+   'address' : '{{location.x}}, {{location.y}}',
+   'price' : 0,
+   'type' : ['palace', 'flat', 'house', 'bungalow'],
+   'rooms' : 1,
+   'guests' : 1,
+   'checkin' : ['12:00', '13:00', '14:00'],
+   'checkout' : ['12:00', '13:00', '14:00'],
+   'features' : ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'],
+   'description' : '',
+   'photos' : ['http://o0.github.io/assets/images/tokyo/hotel1.jpg',
+   'http://o0.github.io/assets/images/tokyo/hotel2.jpg','http://o0.github.io/assets/images/tokyo/hotel3.jpg'],
+   'location' : {x, y},
+  
+  
+  
+  }, {}, {},
+];
