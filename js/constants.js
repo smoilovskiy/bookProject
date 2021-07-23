6d3ddaeb5d4923d49f9515e632f1086852d97a7b
Rostislav Smoilovskiy <smoilovskiy@gmail.com>
2021-07-23 21:33:12 +0300

    add cloned elements

--- js/constants.js
+++ js/constants.js
@@ -9,7 +9,7 @@ export const N_GUESTS_MIN = 1, N_GUESTS_MAX = 10; //number of guests
 export const T_START = 0, T_END = 2; //index of time in times array
 export const FEATURE_INDEX_START = 0, FEATURE_INDEX_END = 5; //index of feature in features array
 export const PHOTO_INDEX_START = 0, PHOTO_INDEX_END = 2; //index of photo in photos array
-export const TITLES = ['заголовок1', 'заголовок2', 'заголовок3', 'заголовок4', 'заголовок5', 'заголовок6', 'заголовок7', 'заголовок8', 'заголовок9', 'заголовок10'];
+export const TITLES = ['Жильё №1', 'Жильё №2', 'Жильё №3', 'Жильё №4', 'Жильё №5', 'Жильё №6', 'Жильё №7', 'Жильё №8', 'Жильё №9', 'Жильё №10'];
 export const TYPES = ['palace', 'flat', 'house', 'bungalow'];
 export const TIMES = ['12:00', '13:00', '14:00'];
 export const FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
--- js/main.js
+++ js/main.js
@@ -1,3 +1,33 @@
 import { offers } from './offersGen.js';
 
+const firstOffer = offers[0]; 
+
+const card = document.querySelector('#card')
+  .content
+  .querySelector('.popup');
+  const clonedCard = card.cloneNode(true);
+
+let title = clonedCard.querySelector('.popup__title');
+title.innerText = firstOffer.offer.title;
+
+let address = clonedCard.querySelector('.popup__text--address');
+let longitude = firstOffer.offer.address.x;
+let latitude = firstOffer.offer.address.y;
+address.innerText = 'Адрес: ' + longitude + ' ' + latitude;
+
+let price = clonedCard.querySelector('.popup__text--price');
+price.innerText = firstOffer.offer.price + '$ / ночь';
+
+
+
+
+
+
+
+
+let map = document.querySelector('.map__canvas');
+map.append(clonedCard);
+
+
+console.log(offers[0]);
 console.log(offers);
