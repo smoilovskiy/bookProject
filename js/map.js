b35fd297dd23bf921eb1db8654c5013055f8e915
Rostislav Smoilovskiy <smoilovskiy@gmail.com>
2021-07-30 21:04:59 +0300

    add main marker

--- js/map.js
+++ js/map.js
@@ -13,4 +13,18 @@ const map = new L.map("map-canvas", mapOptions);
 const layer = new L.TileLayer("http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png");
 map.addLayer(layer);
 
+
+var mainPin = L.icon({
+  iconUrl: './img/main-pin.svg',
+  iconSize: [80, 160],
+  iconAnchor: [22, 94],
+  popupAnchor: [-3, -76],
+});
+
+L.marker([LAT, LNG], {
+  icon: mainPin,
+  draggable: true,
+  
+}).addTo(map);
+
 }
