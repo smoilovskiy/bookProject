7ee04400c9cc2d53ffa64ea7c949bcbdb80f0591
Rostislav Smoilovskiy <smoilovskiy@gmail.com>
2021-08-25 20:48:01 +0300

    add code for remove blue markers when filter is changed

--- js/filter.js
+++ js/filter.js
@@ -1,5 +1,6 @@
 import { offers } from "./offersGen.js";
 import { getData } from "./server.js";
+// import { createCommonMarkers } from "./map.js";
 export const data = 1
 
 // createOffer(serverAnswer[0]);
@@ -9,7 +10,16 @@ formFilter.addEventListener("change", (evt) => {
   getData()
     .then(function (serverAnswer) {
       console.log(serverAnswer);
-      mapFilters(serverAnswer)
+      mapFilters(serverAnswer);
+
+      let commonMarkers = document.querySelectorAll('.leaflet-marker-icon[src="./img/pin.svg"]')
+      console.log(commonMarkers);
+      for (let i = 0; i < commonMarkers.length; i++) {
+        commonMarkers[i].remove();
+      }
+      
+
+
     });
 });
 
@@ -39,11 +49,11 @@ function mapFilters(serverAnswer) {
   console.log(tempArr);
 
 
-  console.log(housingType.value);
-  console.log(housingPrice.value);
-  console.log(housingRooms.value);
-  console.log(housingGuests.value);
-  console.log(filterFeatures);
+  // console.log(housingType.value);
+  // console.log(housingPrice.value);
+  // console.log(housingRooms.value);
+  // console.log(housingGuests.value);
+  // console.log(filterFeatures);
 
   function filterByHousingType(offer) {
     // debugger
@@ -77,8 +87,47 @@ function mapFilters(serverAnswer) {
   }
 
 
-  let filteredArr = serverAnswer.filter(filterByHousingGuests); // filterByHousingType && filterByHousingPrice && filterByHousingRooms && filterByHousingGuests
-  console.log("Выходной массив  " + filteredArr);
+  let filteredArr = serverAnswer.filter(filterByHousingPrice); // filterByHousingType && filterByHousingPrice && filterByHousingRooms && filterByHousingGuests
+  console.log(filteredArr);
+
+//  createCommonMarkers(filteredArr);
+
+  // function createCommonMarkers(serverAnswer) {
+  //   const LAT = 35.68525;
+  //   const LNG = 139.75146;
+  //   const mapZoom = 10;
+  //   const mapOptions = {
+  //     center: [LAT, LNG],
+  //     zoom: mapZoom
+  //   }
+  //   const map = L.map("map-canvas", mapOptions);
+  //   const layer = L.TileLayer("http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png");
+  //   map.addLayer(layer);
+
+  //   for (let i = 0; i < serverAnswer.length; i++) {
+  //     let coordX = serverAnswer[i].location.lat;
+  //     let coordY = serverAnswer[i].location.lng;
+  //     createPins(coordX, coordY, serverAnswer[i]);
+  //     // console.log(serverAnswer[i]);
+  //   }
+
+  //   function createPins(lat, lng, offer) {
+  //     const commonPin = L.icon({
+  //       iconUrl: './img/pin.svg',
+  //       iconSize: [50, 50],
+  //     });
+
+  //     const commonMarker = L.marker([lat, lng], {
+  //       icon: commonPin,
+  //     })
+  //     let markUpCard = createOffer(offer);
+  //     commonMarker.bindPopup(markUpCard).openPopup();
+  //     commonMarker.addTo(map);
+  //     return serverAnswer;
+
+  //   }
+
+  // }
 }
 
 
